447affa4528be38d2e732d04ad31d4d093ae869d
Thales O <98899243+ThalesVonMilet@users.noreply.github.com>
2022-09-16 17:39:59 +0200

    first idea

--- lib/src/local_database/isar/isar_pending.dart
+++ lib/src/local_database/isar/isar_pending.dart
@@ -1,7 +1,44 @@
 
-class IsarPending{
+class PendingRepository {
+  final Isar isarPending;
 
-  Future<void> init() async{
-
+  PendingRepository() {
   }
-}
+
+  init() async {
+    InternetConnectionCheckerPlus().onStatusChange.listen((status) {
+      print(status);
+      StreamSubscription? isarPendingStream;
+      if (status == InternetConnectionStatus.connected) {
+        if(isarPendingStream == null) {
+          isarPendingStream =
+              isarPending.userModels.watchLazy().listen((event) {
+                syncUsersWithServer();
+              });
+        }else{
+          if(isarPendingStream.isPaused) {
+            isarPendingStream.resume();
+          }
+        }
+      }else{
+        if(isarPendingStream != null){
+          isarPendingStream.pause();
+        }
+      }
+    });
+  }
+
+  syncUsersWithServer() async {
+    InternetConnectionStatus status = await InternetConnectionCheckerPlus().connectionStatus;
+    if(status == InternetConnectionStatus.connected){
+      List<UserModel> userModels =
+      await isarPending.userModels.where().findAll();
+      userModels.first;
+      // response = await backend method;
+      bool wasComitedToServer = await ref.read(appwriteRepositoryProvider).createUser(userModels.first);
+      // response success = delete object
+      if (wasComitedToServer) {
+        isarPending.userModels.delete(userModels.first.id!);
+      }
+    }
+  }
